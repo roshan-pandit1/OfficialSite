8ebc956049529e2e97cb8b5409527aa4b498c530
Samarth Rajput <ssamarth224@gmail.com>
2024-03-08 01:56:10 +0530

    responsiveness of navbar

--- assets/js/script.js
+++ assets/js/script.js
@@ -22,16 +22,47 @@ window.onload = function() {
     typeWriter();
 };
 
+//navbar responsive starts
 $(document).ready(function() {
-    $(document).scroll(function() {
-        $('section').each(function() {
-            if ($(this).position().top <= $(document).scrollTop()) {
-                $('.navbar a').removeClass('active');
-                $('.navbar a[href="#' + $(this).attr('id') + '"]').addClass('active');
-            }
-        });
-    });
+    $('#menu').click(function () {
+      $(this).toggleClass('fa-times');
+      $('.navbar').toggleClass('nav-toggle');
+  });
+
+  $(window).on('scroll load', function () {
+      $('#menu').removeClass('fa-times');
+      $('.navbar').removeClass('nav-toggle');
+
+      if (window.scrollY > 60) {
+          document.querySelector('#scroll-top').classList.add('active');
+      } else {
+          document.querySelector('#scroll-top').classList.remove('active');
+      }
+
+      // scroll spy
+      $('section').each(function () {
+          let height = $(this).height();
+          let offset = $(this).offset().top - 200;
+          let top = $(window).scrollTop();
+          let id = $(this).attr('id');
+
+          if (top > offset && top < offset + height) {
+              $('.navbar ul li a').removeClass('active');
+              $('.navbar').find(`[href="#${id}"]`).addClass('active');
+          }
+      });
+  });
+
+  // smooth scrolling
+  $('a[href*="#"]').on('click', function (e) {
+      e.preventDefault();
+      $('html, body').animate({
+          scrollTop: $($(this).attr('href')).offset().top,
+      }, 500, 'linear')
+  });
+
 });
+//navbar responsive ends
 
 // Contact Us form
 //for mail
