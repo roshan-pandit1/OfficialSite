e838f5b89f9030159e12341f08a9a824dad6a5a2
Samarth Rajput <ssamarth224@gmail.com>
2024-03-22 13:10:41 +0530

    fixed all the major errors, added glass effect in the navbar

--- assets/js/members.js
+++ assets/js/members.js
@@ -61,12 +61,12 @@ $(window).on('scroll load', function () {
 });
 
 // smooth scrolling
-$('a[href*="#"]').on('click', function (e) {
-    e.preventDefault();
-    $('html, body').animate({
-        scrollTop: $($(this).attr('href')).offset().top,
-    }, 500, 'linear')
-});
+// $('a[href*="#"]').on('click', function (e) {
+//     e.preventDefault();
+//     $('html, body').animate({
+//         scrollTop: $($(this).attr('href')).offset().top,
+//     }, 500, 'linear')
+// });
 
 });
 //navbar responsive ends
